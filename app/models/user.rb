a91df2d69767edc00b7eef32e5530506af51a0aa
Dmitri Zagidulin <dzagidulin@gmail.com>
2013-12-13 16:13:43 -0500

    Pass user controller tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,36 @@
+require 'riak_json'
+require 'riak_jason_rails'
+require 'active_model'
+
 class User
-  @@collection_name = 'users'
+  include ActiveModel::Model
+  include ActiveModel::Validations
+  include ActiveModel::SecurePassword
+  include RiakJasonRails
+  
+  attr_accessor :key
+  attr_accessor :username, :password_digest
+  attr_accessor :password
+  
+  has_secure_password validations: false
+  
+  def initialize(options={})
+    @key = options.fetch(:key, nil)
+    @username = options.fetch(:username, nil)
+  end
+  
+  def save
+    doc = RippleJson::Document.new(self.key)
+    doc.body = {username: self.username, password_digest: self.password_digest}
+    self.collection.insert(doc)
+  end
+  
+  def self.collection_name
+    'users'
+  end
   
   def self.all
-    collection = self.collection
-    query = {username: {'$regex' => "/.*/"}}.to_json
-    docs = collection.find(query)
-    docs.documents
-  end
-  
-  def self.collection
-    client = RiakJson::Client.new
-    collection = client.collection(@@collection_name)
-  end
-  
-  def self.find(user_key)
-    self.collection.find_by_key(user_key)
+    self.all_for_field('username')
   end
   
   def self.find_by_username(username)
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,5 +5,24 @@ class UserTest < ActiveSupport::TestCase
     it "can be initialized" do
       user = User.new
     end
+    
+    it "has a collection name" do
+      User.collection_name.must_equal 'users'
+    end
+    
+    it "implements a find (by key) method" do
+      user = User.find('abe')
+      user.key = 'abe'
+    end
+    
+    it "implements an all() method" do
+      users = User.all
+      users.wont_be_empty
+    end
+    
+    it "implements a save() instance method" do
+      user = User.new(username: 'george')
+      
+    end
   end
 end
