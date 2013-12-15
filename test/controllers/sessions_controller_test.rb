7d9288d3ab91a1df25ca4212b20e44a4ea37e7ec
Dmitri Zagidulin <dzagidulin@gmail.com>
2013-12-15 12:37:58 -0500

    Red - failing login controller task

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -11,6 +11,11 @@ class SessionsControllerTest < ActionController::TestCase
     assert_redirected_to login_path
   end
 
+  test "Logging in with valid username/password should redirect user to /home" do
+    post :create, user: { username: 'registered_user', password: '12345' }
+    assert_redirected_to user_home_index_path
+  end
+  
   test "Logging out should redirect user to Welcome index" do
     get :destroy
     assert_redirected_to welcome_index_path
