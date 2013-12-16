3173e9eb10826ad7766b7874a100097f45bc5f8b
Dmitri Zagidulin <dzagidulin@gmail.com>
2013-12-16 11:00:08 -0500

    Add forums routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ RiakThreadedForum::Application.routes.draw do
   get "home" => "user_home#index"
   get "user_home/index"
   
+  resources :forums
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
