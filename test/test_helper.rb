19efe43b13c45416995d957e40d2dd0b0c6d9bc6
Dmitri Zagidulin <dzagidulin@gmail.com>
2013-12-18 18:02:05 -0500

    Add login/registered user helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,4 +4,13 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
+  def registered_user
+    user = User.new(username: 'registered_user', password: '12345', password_digest: BCrypt::Password.create('12345'))
+    user.key = 'user-1'
+    user
+  end
+  
+  def login_as(user_key)
+    session[:current_user_key] = user_key
+  end
 end
